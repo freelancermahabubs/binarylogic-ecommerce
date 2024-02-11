42cd120f858e20aef7de72e875079a5f5fdcdee5
smshaown <smshaown65@gmail.com>
2024-02-11 08:08:00 +0600

    updated the code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,25 +1,30 @@
-$(document).ready(function(){
-  
-    $('#categoryCarousel').owlCarousel({
-      loop: true,
-        animateOut: "fadeOut",
-        animateIn: "fadeIn",
-        margin: 14,
-        nav: false,
-        smartSpeed: 500,
-        autoplay: true,
-        autoplayTimeout: 7000,
-      responsive:{
-        0:{
-          items:3,
-        },
-        600:{
-          items:5,
-        },
-        1000:{
-          items:10,
-        }
-      }
-    });
+
+  // banner swiper start 
+  var swiper = new Swiper(".bannerSwiper", {});
+
+  var swiper = new Swiper(".our_brand_slider", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
   });
-  
